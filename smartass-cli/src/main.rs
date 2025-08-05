80263a4ad9d5d95434c25e5e4d31f235c094884a
Mark Schmale <masch@masch.it>
2025-08-05 21:34:38 +0200

    remove gix, call the local git binary directly
    easier to get a proper diff this way, and we can expect git to be there

--- smartass-cli/src/main.rs
+++ smartass-cli/src/main.rs
@@ -6,18 +6,29 @@ struct Arguments {
     compare: String,
 }
 
+fn get_diff(from: &str, to: &str) -> Result<String, ()> {
+    let diff_cmd = std::process::Command::new("git")
+        .arg("--no-pager")
+        .arg("diff")
+        .arg("--no-color")
+        .arg(from)
+        .arg(to)
+        .spawn()
+        .unwrap();
+
+    let output = diff_cmd.wait_with_output().unwrap();
+
+    if !output.status.success() {
+        eprintln!("{}", String::from_utf8_lossy(&output.stderr));
+        return Err(());
+    }
+
+    return Ok(String::from_utf8_lossy(&output.stdout).to_string());
+}
+
 fn main() {
     let args = Arguments::parse();
 
-    let dir = std::env::current_dir().unwrap();
-    let repo = gix::open(dbg!(dir)).unwrap();
-
-    let base_rev = repo.rev_parse(args.base.as_str()).unwrap();
-    let compare_rev = repo.rev_parse(args.compare.as_str()).unwrap();
-
-    if base_rev == compare_rev {
-        eprintln!("cannot compare the same revision");
-    }
-
-    dbg!((base_rev, compare_rev));
+    let output = get_diff(&args.base, &args.compare).unwrap();
+    dbg!(output);
 }
