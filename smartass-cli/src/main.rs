9cb7a25d77403cce65ebac00c5bbd52ca483e960
Mark Schmale <masch@masch.it>
2025-08-05 20:44:26 +0200

    setup basics for gix and clap

--- smartass-cli/src/main.rs
+++ smartass-cli/src/main.rs
@@ -1,3 +1,23 @@
+use clap::Parser;
+
+#[derive(Debug, clap::Parser)]
+struct Arguments {
+    base: String,
+    compare: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Arguments::parse();
+
+    let dir = std::env::current_dir().unwrap();
+    let repo = gix::open(dbg!(dir)).unwrap();
+
+    let base_rev = repo.rev_parse(args.base.as_str()).unwrap();
+    let compare_rev = repo.rev_parse(args.compare.as_str()).unwrap();
+
+    if base_rev == compare_rev {
+        eprintln!("cannot compare the same revision");
+    }
+
+    dbg!((base_rev, compare_rev));
 }
