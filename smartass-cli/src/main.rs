9b919d82e16c06fd6ed4574e9d145f805e786bbe
Mark Schmale <masch@masch.it>
2025-08-05 22:20:42 +0200

    add .ignore support for the review
    this way, we can ignore certain files in the review.
    Usefull for files that may contain large diffs that do not provide
    usefull information, for example Cargo.lock.

--- smartass-cli/src/main.rs
+++ smartass-cli/src/main.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
 use llm::{self, chat::ChatMessage};
-use std::process::Stdio;
+use std::{process::Stdio, slice};
 
 #[derive(Debug, clap::Parser)]
 struct Arguments {
@@ -8,13 +8,47 @@ struct Arguments {
     compare: String,
 }
 
-fn get_diff(from: &str, to: &str) -> Result<String, ()> {
+fn get_change_files(from: &str, to: &str) -> Result<Vec<String>, ()> {
+    let diff_cmd = std::process::Command::new("git")
+        .arg("--no-pager")
+        .arg("diff")
+        .arg("--no-color")
+        .arg("--name-only")
+        .arg(from)
+        .arg(to)
+        .stdin(Stdio::null())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    let output = diff_cmd.wait_with_output().unwrap();
+
+    if !output.status.success() {
+        eprintln!("{}", String::from_utf8_lossy(&output.stderr));
+        return Err(());
+    }
+
+    let stdout = String::from_utf8_lossy(&output.stdout).to_string();
+    let files = stdout
+        .split('\n')
+        .map(|l| l.trim())
+        .filter(|l| !l.is_empty())
+        .map(|l| l.to_owned())
+        .collect();
+
+    Ok(files)
+}
+
+fn get_diff(from: &str, to: &str, files: Vec<&str>) -> Result<String, ()> {
     let diff_cmd = std::process::Command::new("git")
         .arg("--no-pager")
         .arg("diff")
         .arg("--no-color")
         .arg(from)
         .arg(to)
+        .arg("--")
+        .args(files)
         .stdin(Stdio::null())
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
@@ -35,7 +69,23 @@ fn get_diff(from: &str, to: &str) -> Result<String, ()> {
 async fn main() {
     dotenv::dotenv().expect("failed to parse env file");
     let args = Arguments::parse();
-    let output = get_diff(&args.base, &args.compare).unwrap();
+
+    let files = dbg!(get_change_files(&args.base, &args.compare).unwrap());
+
+    let mut filter_builder =
+        ignore::gitignore::GitignoreBuilder::new(std::env::current_dir().unwrap());
+    if let Some(errs) = filter_builder.add("smartass.ignore") {
+        panic!("Failed to add ignore file: {:?}", errs);
+    }
+    let filter = filter_builder.build().unwrap();
+
+    let filtered_files = files
+        .iter()
+        .filter(|file| filter.matched(file, false).is_none())
+        .map(|file| file.as_str())
+        .collect::<Vec<_>>();
+
+    let output = get_diff(&args.base, &args.compare, dbg!(filtered_files)).unwrap();
     dbg!(&output);
 
     let api = llm::builder::LLMBuilder::new()
@@ -53,5 +103,6 @@ async fn main() {
     ];
 
     let output = api.chat(&chat).await.unwrap();
+
     dbg!(output);
 }
